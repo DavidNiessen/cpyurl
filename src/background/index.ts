21de907f3d543a587e3f12adace14b8044e7d569
David Niessen <68539499+DavidNiessen@users.noreply.github.com>
2025-08-29 16:34:41 +0200

    cleanup

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,14 +1,7 @@
+import { copyUrl } from '../index.js'
+
 chrome.commands.onCommand.addListener(async (command) => {
   if (command === 'copy-url') {
-    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
-    if (tab && tab.id && tab.url) {
-      await chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        func: (url: string) => {
-          navigator.clipboard.writeText(url)
-        },
-        args: [tab.url],
-      })
-    }
+    await copyUrl()
   }
 })
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,25 @@
 export const copyUrl = async () => {
-  const currentTabUrl = await getCurrentTabUrl()
+  const currentTab = await getCurrentTab()
 
-  if (currentTabUrl) {
-    await writeToClipboard(currentTabUrl)
+  if (currentTab) {
+    await executeTabScript(currentTab, writeToClipboard)
   }
 }
 
-const getCurrentTabUrl = async () =>
-  (await chrome.tabs.query({ active: true, currentWindow: true })).at(0)?.url
+const executeTabScript = async (tab: any, action: (url: string) => void) => {
+  if (tab.id && tab.url) {
+    await chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: (url: string) => {
+        action(url)
+      },
+      args: [tab.url],
+    })
+  }
+}
+
+const getCurrentTab = async () =>
+  (await chrome.tabs.query({ active: true, currentWindow: true })).at(0)
 
 const writeToClipboard = async (content: string) =>
   await navigator.clipboard.writeText(content)
