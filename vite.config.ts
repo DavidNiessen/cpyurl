ea6f2c6a42864f63a9b2f03e78156a763d78d79b
David Niessen <68539499+DavidNiessen@users.noreply.github.com>
2025-08-30 16:10:11 +0200

    update bundle generation to persist file structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,32 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
+import { relative, resolve } from 'path'
 import copy from 'rollup-plugin-copy'
+import fg from 'fast-glob'
+
+const srcDir = resolve(__dirname, 'src')
+
+const tsFiles = fg.sync('**/*.ts', { cwd: srcDir }).reduce(
+  (obj, file) => {
+    const key = file.replace(/\.ts$/, '')
+    obj[key] = resolve(srcDir, file)
+    return obj
+  },
+  {} as Record<string, string>,
+)
 
 export default defineConfig({
   build: {
     outDir: 'dist/cpyurl',
     rollupOptions: {
-      input: {
-        background: resolve(__dirname, 'src/background/index.ts'),
-        notification: resolve(__dirname, 'src/notification/index.ts'),
-      },
+      input: tsFiles,
       output: {
-        entryFileNames: '[name].js', // Ensures output files are named background.js, notification.js, etc.
+        entryFileNames: (chunkInfo) => {
+          const relativePath = relative(srcDir, chunkInfo.facadeModuleId!)
+          return relativePath.replace(/\.ts$/, '.js')
+        },
       },
       plugins: [
+        // @ts-ignore
         copy({
           targets: [
             { src: 'manifest.json', dest: 'dist/cpyurl' },
